7e6c3fdcf4076217a58dc9a2f212b33f5bf46bb7
RyanDoesCG <macbook@macbooks-MacBook-Air.local>
2023-02-09 15:14:02 +0000

    changing demo scene

--- js/main.js
+++ js/main.js
@@ -59,17 +59,24 @@
         scene.add(new SceneObject(
             "Dragon",
             new Primitive(gl, await MeshGeometry('models/dragon.obj')),
-            new Transform([1.0, 1.0, 1.0], [0.0, 0.0, 0.0], [0.0, 0.2, 0.0]),
-            DefaultMaterial,
+            new Transform([1.0, 1.0, 1.0], [4.0, 0.0, 0.0], [0.0, 0.5, 0.0]),
+            DarkGreyMaterial,
             false))
-/*
+
         scene.add(new SceneObject(
             "Dragon",
             new Primitive(gl, await MeshGeometry('models/AngelWood.obj')),
-            new Transform([1.0, 1.0, 1.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]),
-            DefaultMaterial,
+            new Transform([1.0, 1.0, 1.0], [0.0, 0.0, -4.0], [0.0, -0.5, 0.0]),
+            DarkGreyMaterial,
             false))
-*/
+
+        scene.add(new SceneObject(
+            "Dragon",
+            new Primitive(gl, await MeshGeometry('models/statue.obj')),
+            new Transform([0.05, 0.05, 0.05], [-4.0, 0.0, 0.0], [0.0, -1.0, 0.0]),
+            DarkGreyMaterial,
+            false))
+
         scene.addLight(new DirectionalLight(
             "DirectionaLight",
             new Transform([1.0, 1.0, 1.0], [0.0, 10.0, 10.0], [0.32, 0.0, 0.0]),
